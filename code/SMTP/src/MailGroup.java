e7fcdc05096af21db53941217fd2665ae01a7e64
Istomine <swagman@swag.com>
2023-11-16 14:38:46 +0100

    creation of files

---
+++ code/SMTP/src/MailGroup.java
@@ -0,0 +1,58 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.io.*;
+import java.nio.charset.Charset;
+
+public class MailGroup {
+    private final File file;
+
+    private final int peoplePerGroupe;
+
+
+    public MailGroup(String file, int number){
+        this.file = new File(file);
+        this.peoplePerGroupe = number;
+    }
+
+
+    public ArrayList<ArrayList<String>> getMails(Charset encoding){
+
+        ArrayList<ArrayList<String>> Mails = new ArrayList<>();
+
+        try( var read = new BufferedReader(new InputStreamReader( new FileInputStream(file),encoding ))) {
+
+            String line = read.readLine();
+            int j = 0;
+            while( line != null){
+                Mails.add(new ArrayList<>());
+
+                for(int i = 0 ; i < peoplePerGroupe; ++i){
+                    Mails.get(j).add(line);
+
+                    line = read.readLine();
+                }
+
+                ++j;
+
+            }
+
+
+
+
+
+
+            read.close();
+
+            return output.toString();
+
+        } catch (IOException e) {
+            System.out.println("Exception : " + e);
+            return null;
+        }
+
+
+    }
+
+
+
+}
---
+++ code/SMTP/src/Message.java
@@ -0,0 +1,22 @@
+import java.io.File;
+import java.util.ArrayList;
+
+public class Message {
+
+    private final File folder;
+
+    private ArrayList<String> messageList;
+
+    public Message(String folder){
+        this.folder = new File(folder);
+    }
+
+    /*
+    public ArrayList<String> getMessage(){
+
+    }
+*/
+
+
+
+}
